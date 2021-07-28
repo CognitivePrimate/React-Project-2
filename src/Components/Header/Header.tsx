67af9b2210adae8f07b33b458dbee91c5ef8146e
Tami Fedrigo <tamifedrigo@tamis-mbp.lan>
2021-07-28 18:33:05 -0400

    Header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,29 @@
+import { Link, NavLink } from "react-router-dom";
 
 
-function Header(){
+function Header() {
 
     return (
-        <h1>Header component</h1>
-    )
+        <header className="Header">
+            <Link to="/"><h1>What's for Dinner Recipe Roundup</h1></Link>
+
+            <div className="Nav">
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/detailedItem">Detailed Items</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/favorite">Favorites</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/foundItem">Found Items Items</NavLink>
+                    </li>
+                </ul>
+            </nav>
+            </div>
+        </header>
+    );
 
 }
 
