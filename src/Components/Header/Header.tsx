33b22e259e2bf11f650825a29f3b49d0b8ad1c02
Austin Whitbeck <austinwhitbeckart@gmail.com>
2021-07-28 21:16:15 -0400

    austin commit 3

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
 import { Link, NavLink } from "react-router-dom";
-=======
-import { useState } from "react";
->>>>>>> 72d58e3bc4a7dc793f72bbfd3edcb1a7785b56cb
 
 
 function Header() {
 
 
     return (
-<<<<<<< HEAD
+
         <header className="Header">
             <Link to="/"><h1>What's for Dinner Recipe Roundup</h1></Link>
 
@@ -30,12 +26,6 @@ function Header() {
             </div>
         </header>
     );
-=======
-        <div className="Header">
-     
-        </div>
-    )
->>>>>>> 72d58e3bc4a7dc793f72bbfd3edcb1a7785b56cb
 
 }
 
