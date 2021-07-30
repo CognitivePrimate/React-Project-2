566e389ab999310057fb38f2cac178c5f96af8a1
Derek Jackson <d4jackson84@gmail.com>
2021-07-30 16:55:05 -0400

    details details

--- src/Components/DetailedItem/DetailedItem.tsx
+++ src/Components/DetailedItem/DetailedItem.tsx
@@ -23,21 +23,51 @@ function DetailedItem(){
     // image variable
     let background = `${foundItem?.recipe.image}`
 
+    // changing hyphenated healthLabels from API to spaces instead
+    // let healthLabelFormatting = () => {
+        const healthLabel: string[] | undefined = foundItem?.recipe.healthLabels;
+        const healthLabelSplit: string | undefined = healthLabel?.join(", ");
+        
+    
+
     return (
-        <div className="DetailedItemWrapper" style={{backgroundImage: `url(${background})`}}>
-            <h2>{foundItem?.recipe.label}</h2>
-            <div className="DetailedItemWrapperLeft">
-                {/* <img src={foundItem?.recipe.image} alt="food-pic"/> */}
-            </div>
-            <div className="DetailedItemWrapperRight">
-                <p>Servings: <div className="DetailedNumberBubble">{foundItem?.recipe.yield}</div></p>
-                <p>Calories: <div className="DetailedNumberBubble">{foundItem?.recipe.calories}</div></p>
-            </div>
+        <div className="DetailedItemWrapperImage" style={{backgroundImage: `url(${background})`}}>
+            <div className="DetailedItemWrapper">
+                <div className="DetailedItemImageAndTitleWrapper">
+                    <img className="HiddenImage"src={foundItem?.recipe.image} alt="food-pic"/>
+                    <h2 className="DetailedHeaderText">{foundItem?.recipe.label}</h2>
+                </div>
+                <div className="DetailsWrapper">
+                    <div className="DetailedItemInfoWrapper">
+                        <p className="ItemInfoBit DetailedItemText">Servings</p>
+                        <p className="ItemInfoBit DetailedItemText">Calories</p>
+                        <p className="ItemInfoBit DetailedItemText">PrepTime</p>
+                    </div>
+                    <div className="DetailedItemBubbleWrapper">
+                        <div className="DetailedNumberBubble">{foundItem?.recipe.yield}</div>
+                        <div className="DetailedNumberBubble">{(foundItem ? (foundItem.recipe.calories / foundItem.recipe.yield).toFixed(0) : "")}</div>
+                        <div className="DetailedNumberBubble">{foundItem?.recipe.totalTime}</div>
+                    </div>
+                </div>
+                <div className="DetailedItemDietLabels">
+                    {/* VV if dietlabels exist, display below. otherwise nothing displays */}
+                    {foundItem?.recipe.dietLabels ? <p className="DetailedItemText"><b className="DetailedItemText">Diet Labels:</b> {foundItem?.recipe.dietLabels}</p> : null}
+                    <p className="DetailedItemText"><b className="DetailedItemText">Health Labels: </b>{healthLabelSplit}</p>
+                </div>
+                <div className="DetailedItemIngredients">
+                    <p className="DetailedItemText"><b className="DetailedItemText">Ingredients: </b>{foundItem?.recipe.ingredientLines}</p>
+                </div>
+                <div className="DetailedItemFooter">
 
-
+                </div>
+            </div>
         </div>
     )
-
 }
+    
+    // url: string;
+    // ingredientLines: string[];
+    // favorite: boolean;
+
 
 export default DetailedItem;
