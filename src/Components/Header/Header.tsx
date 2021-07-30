e1d7153154a2835a64e39c17ad5fd06ecdb8c25a
Tami Fedrigo <tamifedrigo@Tamis-MacBook-Pro.local>
2021-07-29 20:44:01 -0400

    Header Updates

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,4 +1,6 @@
 import { Link, NavLink } from "react-router-dom";
+import { VscHeart } from 'react-icons/vsc';
+
 
 
 function Header() {
@@ -9,19 +11,13 @@ function Header() {
 
 
         <header className="Header">
-            <Link to="/"><h1>What's for Dinner Recipe Roundup</h1></Link>
+            <Link to="/" ><h1>What's for Dinner?</h1></Link>
 
             <div className="Nav">
                 <nav>
                     <ul>
                         <li>
-                            <NavLink to="/detailedItem">Detailed Items</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/favorite">Favorites</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/foundItem">Found Items Items</NavLink>
+                            <NavLink to="/favorite">Favorites <VscHeart /></NavLink>
                         </li>
                     </ul>
                 </nav>
