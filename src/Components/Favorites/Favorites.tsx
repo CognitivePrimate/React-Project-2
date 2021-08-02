5d644b0f308b6049aa8dd7db704675a0e57dd294
Tami Fedrigo <tamifedrigo@tamis-mbp.lan>
2021-08-02 10:37:22 -0400

    Favorites attemptfail

--- src/Components/Favorites/Favorites.tsx
+++ src/Components/Favorites/Favorites.tsx
@@ -1,13 +1,32 @@
+import { useContext } from "react";
+import { ItemContext } from "../../Context/ItemContextProvider";
+import Item from "../Item/Item";
 
 
 function Favorites(){
-
+    const {items, favorites} = useContext(ItemContext);
     
-
     return (
-        <h3>favorites component</h3>
+        
+        <div className="FavoritesWrapper">  
+        <ol>
+            {Favorites.map(favorites, index) => {
+                <li>{favorites.text}</li>
+            }
+            
+            </ol>
+        </div>
     )
 
 }
 
+ 
+
 export default Favorites;
+
+ 
+
+function index(favorites: import("../../Model/ItemInterface").Item[], index: any): string | number | boolean | {} | import("react").ReactElement<any, string | import("react").JSXElementConstructor<any>> | import("react").ReactNodeArray | import("react").ReactPortal | null | undefined {
+    throw new Error("Function not implemented.");
+}
+
