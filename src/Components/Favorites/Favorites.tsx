db20ef2111c4c93b68e0ea1223bcbdbd640e17b4
Tami Fedrigo <tamifedrigo@tamis-mbp.lan>
2021-08-02 21:45:49 -0400

    Tami Push

--- src/Components/Favorites/Favorites.tsx
+++ src/Components/Favorites/Favorites.tsx
@@ -1,28 +1,21 @@
-import { useContext } from "react";
-import { ItemContext } from "../../Context/ItemContextProvider";
+// import { useContext } from "react";
+// import { ItemContext } from "../../Context/ItemContextProvider";
 
 
-function Favorites(){
-    const {favorites = useContext(ItemContext);
+// function Favorites(){
+//     const {favorites = useContext(ItemContext);
     
-    return (
+//     return (
         
-        <div className="FavoritesWrapper">  
-        <ul>
-            {Favorites.map((favorites, index) => 
-                <li>{favorites.label}</li>
-            )}
-            </ul>
-        </div>
-    )
-}
+//         <div className="FavoritesWrapper">  
+//         <ul>
+//             {Favorites.map((favorites, index) => 
+//                 <li>{favorites.label}</li>
+//             )}
+//             </ul>
+//         </div>
+//     )
+// }
 
-// {items.map((item, index) => 
-//     <Item 
-//         key={`${item.recipe.label}-${index}`}
-//         item={item.recipe}
-//     />
-// )}
- 
 
-export default Favorites;
+// export default Favorites;
