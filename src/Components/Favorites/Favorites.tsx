01ac344e3509cfed5c2a3916edbc25e9b9ce5c42
Derek Jackson <d4jackson84@gmail.com>
2021-08-04 20:07:45 -0400

    polish time

--- src/Components/Favorites/Favorites.tsx
+++ src/Components/Favorites/Favorites.tsx
@@ -2,14 +2,15 @@ import { useContext } from "react";
 import { ItemContext } from "../../Context/ItemContextProvider";
 import Item from "../Item/Item";
 
+import "./FavoritesStyles.css";
 
 function Favorites(){
     const {favorites} = useContext(ItemContext);
     console.log("favorites from Favorites Component", favorites);
     return (
-        
         <div className="FavoritesWrapper">
-            <p>Favorites Test</p>
+            <p className="PaddingParagraph">""</p>
+            <h2>Favorites</h2>
             {favorites.map((item, index) => 
                 <Item 
                     key={`${item.label}-${index}`}
